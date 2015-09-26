373f1af10b58b44a22ae7abeb75281fda6a3f2db
David Tebbi <davidtebbi@gmail.com>
2015-09-26 14:59:05 -0400

    socket images sending

--- scotch-chat-main-app/app/public/js/video-image-capture/video-state.js
+++
@@ -1,9 +0,0 @@
-app.config(function ($stateProvider) {
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'js/video-image-capture/video.html',
-        controller: 'VideoCtrl'
-    });
-});
-
-
